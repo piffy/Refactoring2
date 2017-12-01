37b0bcae150f6157b5bd8c298569601583aea7ff
Marcello Missiroli <prof.missiroli@gmail.com>
2017-12-01 20:50:36 +0100

    Magic Numbers - 1

--- src/refactoringexe/RefactoringExe.java
+++ src/refactoringexe/RefactoringExe.java
@@ -15,7 +15,6 @@ public class RefactoringExe {
      * @param args the command line arguments
      * PROBLEMI: 
 
-     * Numeri magici. Che sono 1,2,3,4
      * Altri numeri magici. Ce sono 0.1,0.5, 0.7?
      * Ripetizione: vedete codice ripetuto??
      * BUG OSCURO?
@@ -23,25 +22,33 @@ public class RefactoringExe {
      * RISOLTO (tramite Refactor->Rename:
      * Illeggibile. Richiede tempo.
      * Nomi oscuri. Che sono? A che serve questa classe
+     * Numeri magici. Che sono 1,2,3,4
      */
 
   public double applicaSconto(double prezzo, double tipoCliente, double anni)
   {
+        /** assolutamente migliorabile, vedremo come **/
+        final int NONREGISTRATO = 1;
+        final int STANDARD = 2;
+        final int BRONZO = 3;
+        final int ARGENTO = 4;
+      
+      
     double prezzoScontato = 0;
     double sco = (anni > 5) ? (double)5/100 : (double)anni/100; 
-    if (tipoCliente == 1)
+    if (tipoCliente == NONREGISTRATO)
     {
       prezzoScontato = prezzo;
     }
-    else if (tipoCliente == 2)
+    else if (tipoCliente == STANDARD)
     {
       prezzoScontato = (prezzo - (0.1 * prezzo)) - sco * (prezzo - (0.1 * prezzo));
     }
-    else if (tipoCliente == 3)
+    else if (tipoCliente == BRONZO)
     {
       prezzoScontato = (0.7 * prezzo) - sco * (0.7 * prezzo);
     }
-    else if (tipoCliente == 4)
+    else if (tipoCliente == ARGENTO)
     {
       prezzoScontato = (prezzo - (0.5 * prezzo)) - sco * (prezzo - (0.5 * prezzo));
     }
